af7a8d3634fd3dc0741193e1822addda53763892
julitschkaa <schaak@posteo.net>
2021-10-07 22:19:57 +0200

    Yadded model for projects

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+from projects.models import Project
+
+
+def project_index(request):
+    projects = Project.objects.all()
+    context = {
+        'projects': projects
+    }
+
+    return render(request, 'project_index.html', context)
